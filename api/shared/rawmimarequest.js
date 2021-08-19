556475239e42590558a5035cfe31156171280244
vbhenri <vhenri@deloitte.ca>
2021-08-19 14:16:44 -0400

    more tests

--- api/shared/rawmimarequest.js
+++ api/shared/rawmimarequest.js
@@ -26,7 +26,7 @@ const executeSQL = (context, sql) => {
         if (err) {
             context.log.error(err);
             context.res.status = 500;
-            context.res.body = "Error connecting to Azure SQL query" + err + " server: " + process.env.db_server + " user: " + process.env.db_user + " - User? - " + process.env.DB_USER;
+            context.res.body = "Error connecting to Azure SQL query" + err + " server: " + process.env.db_server + " user: " + process.env.db_user + " pass: " + process.env.db_password + " db: " + process.env.db_database;
             context.done();
         }
         else {
