f69d1bcd5e5c33c632db1f912e120563037b8bbf
vbhenri <vhenri@deloitte.ca>
2021-08-19 13:07:45 -0400

    trigger rebuild

--- api/getUsers/index.js
+++ api/getUsers/index.js
@@ -3,7 +3,6 @@ const { executeSQL } = require('../shared/rawmimarequest');
 getUsers = function (context, req) {    
     const method = req.method.toLowerCase();
     var sql = "SELECT * FROM [dbo].[Users] FOR JSON AUTO;"
-
     executeSQL(context, sql)
 }
 
